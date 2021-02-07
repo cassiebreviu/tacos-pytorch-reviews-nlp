38bfa5cf83028e2aed9ce0eef3dad6447aa1f106
Cassie Breviu <cassieb@Microsoft.com>
2021-02-07 14:21:44 -0600

    add mlflow, fix acc and loss, add onnx save

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ from torchtext.datasets import TextClassificationDataset
 from torch.utils.data.dataset import random_split
 import pickle
 import pandas as pd
+import torch.onnx as onnx
+import mlflow
 
 ###################################################################
 # Helpers                                                         #
@@ -43,6 +45,26 @@ def load_vocab(filename):
 
 def array_to_tensor(df_item):
     return torch.tensor(df_item['tensor'])
+
+def save_model_onnx(model, x_model_input_tensor, model_output_path):
+    model.eval()
+    # Export the model
+    onnx.export(model,   # model being run
+    x,                         # model input (or a tuple for multiple inputs)
+    model_output_path,         # where to save the model (can be a file or file-like object)
+    export_params=True,        # store the trained parameter weights inside the model file
+    opset_version=10,          # the ONNX version to export the model to
+    do_constant_folding=True,  # whether to execute constant folding for optimization
+    input_names = ['input'],   # the model's input names
+    output_names = ['output'], # the model's output names
+    dynamic_axes={'input' : {0 : 'batch_size'},    # variable lenght axes
+                  'output' : {0 : 'batch_size'}})
+
+  
+                  
+def load_model_onnx(model_path):
+    onnx_model = onnx.load(model_path)
+    onnx.checker.check_model(onnx_model)
 
 ###################################################################
 # Training                                                        #
@@ -83,8 +105,13 @@ def train_func(train_dataset, batch_size,optimizer, model, criterion, scheduler,
 
     # Adjust the learning rate
     scheduler.step()
+    items_in_dataset = len(train_dataset)
+    print(f'items_in_dataloader: {items_in_dataset}')
 
-    return train_loss / len(train_loader), train_acc / len(train_loader)
+    train_loss = train_loss / items_in_dataset
+    train_acc = train_acc / items_in_dataset
+    print(f'train_loss: {train_loss}, train_acc: {train_acc}')
+    return train_loss, train_acc
 
 ###################################################################
 # Testing                                                         #
@@ -107,7 +134,12 @@ def test(test_dataset, batch_size, model, criterion, device):
             loss += loss.item()
             acc += (output.argmax(1) == cls).sum().item()
 
-    return loss / len(test_loader), acc / len(test_loader)
+    items_in_dataset = len(test_dataset)
+    print(f'items_in_dataloader: {items_in_dataset}')
+
+    val_loss = float(loss) / items_in_dataset
+    val_acc = acc / items_in_dataset
+    return val_loss, val_acc
 
 
 def predict(text, model, vocab, ngrams):
@@ -188,7 +220,7 @@ def main(input_path, output_path, device):
 
     info('Training')
 
-
+    mlflow.start_run()
     for epoch in range(N_EPOCHS):
 
         start_time = time.time()
@@ -203,16 +235,20 @@ def main(input_path, output_path, device):
         print(f'\tLoss: {train_loss:.4f}(train)\t|\tAcc: {train_acc * 100:.1f}%(train)')
         print(f'\tLoss: {valid_loss:.4f}(valid)\t|\tAcc: {valid_acc * 100:.1f}%(valid)')
 
-    # TODO fix this so it actually creates and saves a model to the output path
-    file_output = os.path.join(output_path, 'latest.hdf5')
-    print('Serializing h5 model to:\n{}'.format(file_output))
-    model.save(file_output)
+        # log to mlflow
+        mlflow.log_metric("epoch_loss", train_loss)
+        mlflow.log_metric("epoch_accuracy", train_acc)
+        mlflow.log_metric("val_loss", valid_loss)
+        mlflow.log_metric("val_acc", valid_acc)
 
+    # ml flow is closed here, probably change that
+    print(f'sample tensor {yelp_train_dataset[0]}')
+    x_model_input_tensor = yelp_train_dataset[0]
+    file_output = Path(os.path.join(output_path, "model.onnx")).resolve()
     print(f'Output path => {str(file_output)}')
     print('Writing file to directory... ', end='')
-    with open(file_output, 'wb') as f:
-        f.write()
-    print('done')
+    #model.save(file_output)
+    save_model_onnx(model, x_model_input_tensor, file_output)
     
     print('Output Files:')
     for f in os.listdir(str(output_path)):
@@ -223,7 +259,10 @@ def main(input_path, output_path, device):
     test_loss, test_acc = test(yelp_test_dataset,batch_size, model, criterion, device)
     print('\nTest loss:', test_loss)
     print('\nTest accuracy:', test_acc)
+    mlflow.log_metric("test_loss", test_loss)
+    mlflow.log_metric("test_acc", test_acc)
 
+    mlflow.stop_run()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='test')
     parser.add_argument('-s', '--source_path', help='source directory')
