21741c3e4a10c8a3c576ed84c77cfec1c9aaccaf
sethjuarez <me@sethjuarez.com>
2021-02-08 18:41:48 -0800

    better model save and load for deployment

--- score.py
+++ score.py
@@ -28,12 +28,12 @@ class TextSentiment(nn.Module):
         return F.softmax(self.fc(embedded), dim=1)
 
 # globals
-model, dictionary, tokenizer, ngrams  = None, None, None, 2
+model, dictionary, tokenizer, ngrams, device  = None, None, None, 2, None
 def init():
-    global model, dictionary, tokenizer, ngrams
+    global model, dictionary, tokenizer, ngrams, device
 
-    # scoping issue with pickling
-    binding = TextSentiment
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'Using {device} device!')
 
     try:
         model_files = Model.get_model_path('tacoreviewsmodel')
@@ -42,40 +42,53 @@ def init():
 
     model_path = Path(model_files).resolve()
     print(f'Loading model files from {str(model_path)}')
-
-    model_file = (model_path / 'model.pth').resolve()
+    
+    # load vocabulary
     vocab_file = (model_path / 'vocab.data').resolve()
-
-    model = torch.load(str(model_file))
     dictionary = torch.load(str(vocab_file))
+
+    # load metadata
+    meta_file = (model_path / 'metadata.json').resolve()
+    with open(str(meta_file)) as f:
+        metadata = json.load(f)
+
+    # load model
+    model_file = (model_path / 'model.pth').resolve()
+    vocab_size = metadata['vocab_size']
+    embed_dim = metadata['embed_dim']
+    num_class = metadata['num_class']
+    model = TextSentiment(vocab_size, embed_dim, num_class)
+    model.load_state_dict(torch.load(str(model_file), map_location=device))
+    model.to(device)
+    
     tokenizer = get_tokenizer("basic_english")
 
 
 def run(raw_data):
-    global model, dictionary, tokenizer, ngrams
+    global model, dictionary, tokenizer, ngrams, device
 
     prev_time = time.time()
     post = json.loads(raw_data)
-    text = post['text']
+    incoming_text = post['text']
 
     with torch.no_grad():
         text = torch.tensor([dictionary[token]
-                            for token in ngrams_iterator(tokenizer(text), ngrams)])
-        output = model(text, torch.tensor([0]))
+                            for token in ngrams_iterator(tokenizer(incoming_text), ngrams)])
+
+        output = model(text.to(device), torch.tensor([0]).to(device))
 
         current_time = time.time()
         inference_time = datetime.timedelta(seconds=current_time - prev_time)
 
         payload = {
             'time': str(inference_time.total_seconds()),
-            'scores': output[0],
+            'text': incoming_text,
+            'scores': output[0].tolist(),
             'rating': output.argmax(1).item() + 1
         }
 
         print('Input ({}), Prediction ({})'.format(text, payload))
-
         return payload
-
 
 if __name__ == '__main__':
     init()
